fa9921554927ab6a036f9cfc9468825b6f4b113e
navygo <whj001@126.com>
2019-04-11 13:17:53 +0800

    add 2 ,B.java

--- app/src/main/java/com/study/navy/study/B.java
+++ app/src/main/java/com/study/navy/study/B.java
@@ -1,19 +1,19 @@
 package com.study.navy.study;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class B extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_b);
 
         A a = (A)getApplicationContext();
         String s = a.s;
-        s = a.getString("ok is good idea");
-        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+        s=a.getString("other");
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
